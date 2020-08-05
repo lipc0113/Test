5fcb6a44b6fe609fbb98b3590064e681a554ab0b
v_lipengcheng <v_lipengcheng@baidu.com>
2020-08-05 14:07:16 +0800

    Lambda表达式和成员引用

--- kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/KotlinActivity.kt
+++ kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/KotlinActivity.kt
@@ -1,11 +1,11 @@
 package com.lpc.test.kotlin_lib
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.lpc.test.kotlin_lib.test.Person
+import com.lpc.test.kotlin_lib.topfun.log
 import com.lpc.test.kotlin_lib.utils.LogUtil
 
 /**
@@ -15,35 +15,71 @@ import com.lpc.test.kotlin_lib.utils.LogUtil
  */
 class KotlinActivity : AppCompatActivity() {
 
+    val peoples = listOf(Person("lipc", 30), Person("syj", 28))
     lateinit var tvTest: TextView
     lateinit var tvTest2: TextView
+    lateinit var tvTest3: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin);
         initView()
         initData()
+        initData2()
+        initData3("name")
     }
 
     fun initView() {
+        var clickCount = 0
         tvTest = findViewById<TextView>(R.id.tv_test)
         tvTest.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 LogUtil.d("view=${v}")
+                finish()
             }
         })
 
         tvTest2 = findViewById<TextView>(R.id.tv_test2)
         // 5.1.1作为函数参数的代码块
-        tvTest2.setOnClickListener { Log.d("lipc0113", "view=${tvTest2}") }
+        tvTest2.setOnClickListener {
+            LogUtil.d("view=${tvTest2}")
+            // 5.1.4在作用域中访问变量2
+            clickCount++
+            LogUtil.d("clickCount=$clickCount")
+        }
+
+        tvTest3 = findViewById(R.id.tv_test3)
+        tvTest3.setOnClickListener {
+            LogUtil.d("成员引用")
+            // 成员引用
+            run(::log)
+        }
     }
 
     fun initData() {
-        val peoples = listOf(Person("lipc", 30), Person("syj", 28))
         // 5.1.2Lammbda与集合
         tvTest.setText(peoples.maxBy { it.age }?.name)
         tvTest2.setText(peoples.maxBy(Person::age)?.age.toString())
-        LogUtil.d(peoples.maxBy { it.age }?.name.toString())
-        LogUtil.d(peoples.maxBy(Person::age)?.age.toString())
+    }
+
+    fun initData2() {
+        // 5.1.3Lambda表达式的语法
+        val sum = { x:Int, y:Int-> x+y }
+        LogUtil.d(sum(1,2).toString())
+
+        //Lambda表达式的省略过程优化
+        LogUtil.d("1:::" + peoples.maxBy({ p: Person -> p.age })!!.name);
+        LogUtil.d("2:::" + peoples.maxBy() { p: Person -> p.age }!!.name);
+        LogUtil.d("3:::" + peoples.maxBy { p: Person -> p.age }!!.name);
+        LogUtil.d("4:::" + peoples.maxBy { p -> p.age }!!.name);
+        val test = { p: Person -> p.age }
+        LogUtil.d("4:::" + peoples.maxBy(test)!!.name);
+    }
+
+    fun initData3(prefix: String) {
+        // 5.1.4在作用域中访问变量
+        peoples.forEach{
+            LogUtil.d("$prefix:${it.name}")
+        }
     }
 }
---
+++ kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/topfun/topfun.kt
@@ -0,0 +1,10 @@
+package com.lpc.test.kotlin_lib.topfun
+
+import com.lpc.test.kotlin_lib.utils.LogUtil
+
+/**
+ * @ Author     ：v_lipengcheng
+ * @ Date       ：Created in 13:16 2020/8/5
+ * @ Description：
+ */
+fun log() = LogUtil.d("topfun")

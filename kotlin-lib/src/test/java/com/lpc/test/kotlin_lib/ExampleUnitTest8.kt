f9ea9b576562b5bc87bf78bdac4e9a7dedfbb6c9
v_lipengcheng <v_lipengcheng@baidu.com>
2020-10-15 20:38:26 +0800

    第九章 注解与反射

---
+++ kotlin-lib/src/test/java/com/lpc/test/kotlin_lib/ExampleUnitTest8.kt
@@ -0,0 +1,89 @@
+package com.lpc.test.kotlin_lib
+
+import org.junit.Test
+import java.util.*
+import kotlin.Comparator
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ * 第九章 注解与反射
+ */
+class ExampleUnitTest8 {
+
+    val list = arrayListOf("lipc", "syj")
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect10() {
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect9() {
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect8() {
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect7() {
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect6() {
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect5() {
+
+    }
+
+    /**
+     *
+     * */
+    @Test
+    fun addition_isCorrect4() {
+
+    }
+
+    /**
+     * */
+    @Test
+    fun addition_isCorrect3() {
+    }
+
+    /**
+     * */
+    @Test
+    fun addition_isCorrect2() {
+    }
+
+    /**
+     * */
+    @Test
+    fun addition_isCorrect() {
+
+    }
+
+}
+

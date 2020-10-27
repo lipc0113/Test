b3e475563b74228cf59b2fa6e869837c8f79fccc
lipc0113 <305406786@qq.com>
2020-10-27 22:53:59 +0800

    冒泡排序、选择排序

--- kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/test/AlgorithmActivity.kt
+++ kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/test/AlgorithmActivity.kt
@@ -29,6 +29,109 @@ class AlgorithmActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * 先找小
+     */
+    fun 冒泡排序(view: View) {
+        val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
+
+        var temp = 0
+        var b = false
+        for (i in 0 until arrays.size - 1) {
+            b = false
+            for (j in arrays.size - 1 downTo i + 1) {
+                if (arrays[j] < arrays[j - 1]) {
+                    b = true
+                    temp = arrays[j]
+                    arrays[j] = arrays[j - 1]
+                    arrays[j - 1] = temp
+                }
+            }
+            if (!b) {
+                break
+            }
+        }
+        for (i in arrays.indices) {
+            LogUtil.d("arrays[i] = ${arrays[i]}")
+        }
+    }
+
+    /**
+     * 先找大
+     */
+    fun 冒泡排序2(view: View) {
+        val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
+
+        var temp = 0
+        var b = false
+        for (i in 0 until arrays.size - 1) {
+            b = false
+            for (j in 0 until arrays.size - 1 - i) {
+                if (arrays[j] > arrays[j + 1]) {
+                    b = true
+                    temp = arrays[j]
+                    arrays[j] = arrays[j + 1]
+                    arrays[j + 1] = temp
+                }
+            }
+            if (!b) {
+                break
+            }
+        }
+        for (i in arrays.indices) {
+            LogUtil.d("arrays[i] = ${arrays[i]}")
+        }
+    }
+
+    fun 选择排序(view: View) {
+        val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
+
+        var temp = 0
+        val min = 0
+        var b = false
+        for (i in 0 until arrays.size - 1) {
+            b = false
+            for (j in i + 1 until arrays.size) {
+                if (arrays[j] < arrays[i]) {
+                    b = true
+                    temp = arrays[j]
+                    arrays[j] = arrays[i]
+                    arrays[i] = temp
+                }
+            }
+            if (!b) {
+                break
+            }
+        }
+        for (i in arrays.indices) {
+            LogUtil.d("arrays[i] = ${arrays[i]}")
+        }
+    }
+
+    /**
+     * 选择排序--优化
+     */
+    fun 选择排序2(view: View) {
+        val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
+
+        var temp = 0
+        var min = 0
+        for (i in 0 until arrays.size - 1) {
+            min = i
+            for (j in i + 1 until arrays.size) {
+                if (arrays[j] < arrays[min]) {
+                    min = j
+                }
+            }
+            temp = arrays[min]
+            arrays[min] = arrays[i]
+            arrays[i] = temp
+        }
+        for (i in arrays.indices) {
+            LogUtil.d("arrays[i] = ${arrays[i]}")
+        }
+    }
+
     fun test(view: View) {
         val node = ListNode(10)
         val node2 = ListNode(20)
@@ -39,7 +142,7 @@ class AlgorithmActivity : AppCompatActivity() {
         node3.next = node4
         var sort = sort(node)
         while (sort != null) {
-            println("node = ${sort.value}")
+            LogUtil.d("node = ${sort.value}")
             sort = sort.next
         }
     }

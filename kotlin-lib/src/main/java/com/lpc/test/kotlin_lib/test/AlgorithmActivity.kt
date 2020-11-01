88f5bceee3c1267fd993ac01312af8e6de8c7b70
lipc0113 <305406786@qq.com>
2020-11-01 21:07:31 +0800

    算法：希尔排序

--- kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/test/AlgorithmActivity.kt
+++ kotlin-lib/src/main/java/com/lpc/test/kotlin_lib/test/AlgorithmActivity.kt
@@ -84,35 +84,10 @@ class AlgorithmActivity : AppCompatActivity() {
         }
     }
 
-    fun 选择排序(view: View) {
-        val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
-
-        var temp = 0
-        val min = 0
-        var b = false
-        for (i in 0 until arrays.size - 1) {
-            b = false
-            for (j in i + 1 until arrays.size) {
-                if (arrays[j] < arrays[i]) {
-                    b = true
-                    temp = arrays[j]
-                    arrays[j] = arrays[i]
-                    arrays[i] = temp
-                }
-            }
-            if (!b) {
-                break
-            }
-        }
-        for (i in arrays.indices) {
-            LogUtil.d("arrays[i] = ${arrays[i]}")
-        }
-    }
-
     /**
-     * 选择排序--优化
+     * 选择排序
      */
-    fun 选择排序2(view: View) {
+    fun 选择排序(view: View) {
         val arrays = intArrayOf(10, 1, 4, 5, 0, 9, 2, 3)
 
         var temp = 0
